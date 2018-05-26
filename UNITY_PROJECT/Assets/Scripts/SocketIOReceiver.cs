f83a7c6fbcebb28cd3a66b93cfffc46e253d80a9
Dennis <wittchen@htw-dresden.de>
2018-05-26 15:50:54 +0200

    ADD: WebSocket support for Unity

---
+++ UNITY_PROJECT/Assets/Scripts/SocketIOReceiver.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using SocketIO;
+
+public class SocketIOReceiver : MonoBehaviour {
+
+    private SocketIOComponent socket;
+
+    private void Start() {
+        var socketIO = GameObject.Find("SocketIO");
+        socket = socketIO.GetComponent<SocketIOComponent>();
+        socket.Connect();
+        socket.On("open", OnOpen);
+        socket.On("error", OnError);
+        socket.On("close", OnClose);
+        socket.On("lift", OnLift);
+    }
+
+
+    void OnOpen(SocketIOEvent e) {
+        Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
+    }
+
+
+    void OnError(SocketIOEvent e) {
+        Debug.Log("[SocketIO] Error received: " + e.name + " " + e.data);
+    }
+
+
+    void OnClose(SocketIOEvent e) {
+        Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
+    }
+
+
+    void OnLift(SocketIOEvent e) {
+        var jsonData = e.data;
+        Debug.Log("[SocketIO] lift triggered: " + e.name + " " + jsonData);
+    }
+
+
+    public void AckToServer() {
+        socket.Emit("ack");
+    }
+}
