4c304bd714662aa8f44c8785983272e1c36220c6
Artjom Djaditschkin <belic93@yandex.ru>
2018-05-26 20:17:57 +0200

    * added visualization for mic and added it to a player prefab

---
+++ UNITY_PROJECT/Assets/Scripts/MicVisualizer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicVisualizer : MonoBehaviour
+{
+	private AudioSource audioSource;
+
+	public float[] Samples = new float[512];
+
+	public float [] FreqBands = new float[8];
+
+	// Use this for initialization
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetSpectrumAudioSource();
+		MakeFreqBands();
+	}
+
+	private void GetSpectrumAudioSource()
+	{
+		audioSource.GetSpectrumData(Samples, 0, FFTWindow.Blackman);
+	}
+	
+	private void MakeFreqBands()
+	{
+		int count = 0;
+
+		for (int i = 0; i < 8; i++)
+		{
+			float average = 0;
+			int sampleCount = (int) Mathf.Pow(2, i) * 2;
+			if (i == 7)
+			{
+				sampleCount += 2;
+			}
+
+			for (int j = 0; j < sampleCount; j++)
+			{
+				average += Samples[count] * (count + 1);
+				count++;
+			}
+
+			average /= count;
+
+			FreqBands[i] = average * 10;
+		}
+	}
+}
---
+++ UNITY_PROJECT/Assets/Scripts/ParamCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+
+	public int Band;
+	public float StartScale;
+	public float ScaleMultiplier;
+
+	private MicVisualizer micVisualizer;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		micVisualizer = transform.parent.GetComponentInParent<MicVisualizer>();
+
+		if (micVisualizer == null)
+		{
+			Debug.LogError("Couldn't find mic visualizer!");
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector3(transform.localScale.x, (micVisualizer.FreqBands[Band] * ScaleMultiplier) + StartScale, transform.localScale.z);
+	}
+}
