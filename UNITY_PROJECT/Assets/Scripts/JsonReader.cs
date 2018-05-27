c2f3cda3c8e7d7a6308c7226f2cc311df769e7da
Kay <kaybode@gmx.de>
2018-05-27 17:18:56 +0200

    again refactored reader

--- UNITY_PROJECT/Assets/Scripts/JsonReader.cs
+++ UNITY_PROJECT/Assets/Scripts/JsonReader.cs
@@ -19,53 +19,49 @@ public class Discussion
 }
 
 
-
-
 public class JsonReader : MonoBehaviour
 {
-
-
     public Discussion onJSONDiscussionReceived(JSONObject jsonDiscussion)
     {
         //var keyArray = jsonDiscussion.keys.ToArray();
-        Discussion discussion = null;
+        Discussion discussion = new Discussion();
         List<Member> memberList = null;
         var keyArray = jsonDiscussion.keys.ToArray();
+        Dictionary<string, JSONObject> localMap = new Dictionary<string, JSONObject>();
 
         foreach (var key in keyArray)
         {
-            if (jsonDiscussion[key] != null)
+            if (!localMap.ContainsKey(key))
             {
-                if (jsonDiscussion[key].Equals("title"))
-                {
-                    if (jsonDiscussion[key].type == JSONObject.Type.STRING)
-                    {
-                        Debug.Log("Got title: " + jsonDiscussion[key]);
-                        discussion.title = jsonDiscussion[key].ToString();
-                    }
-                    else
-                    {
-                        Debug.Log("False Datatype " + jsonDiscussion[key]);
-                    }
-                }
-                else if (jsonDiscussion[key].Equals("duration"))
-                {
+                localMap.Add(key, jsonDiscussion[key]);
+            }
 
-                    if (jsonDiscussion[key].type == JSONObject.Type.NUMBER)
+            switch (key)
+            {
+                case "title":
+                    if (localMap[key].type == JSONObject.Type.STRING)
                     {
-                        Debug.Log("Got Duration: " + jsonDiscussion[key]);
-                        discussion.duration = (int)jsonDiscussion[key].n;
+                        Debug.Log("Got Title: " + localMap[key]);
+                        discussion.title = localMap[key].ToString();
+                    }
+                    break;
+                case "duration":
+                    if (localMap[key].type == JSONObject.Type.NUMBER)
+                    {
+                        Debug.Log("Got Duration: " + localMap[key]);
+                        discussion.duration = (int)localMap[key].n;
                     }
                     else
                     {
-                        Debug.Log("False Datatype " + jsonDiscussion[key]);
+                        Debug.Log("False Datatype " + localMap[key]);
                     }
-                }
-                else if (jsonDiscussion[key].Equals("member"))
-                {
-                    if (jsonDiscussion[key].type == JSONObject.Type.ARRAY)
+                    break;
+                case "members":
+                    if (localMap[key].type == JSONObject.Type.ARRAY)
                     {
-                        foreach (JSONObject currentMember in jsonDiscussion[key].list)
+                        Dictionary<string, JSONObject> localMap2 = new Dictionary<string, JSONObject>();
+
+                        foreach (JSONObject currentMember in localMap[key].list)
                         {
                             Member newMember = new Member();
                             var keyArray2 = currentMember.keys.ToArray();
@@ -73,71 +69,73 @@ public class JsonReader : MonoBehaviour
 
                             foreach (var key2 in keyArray2)
                             {
-                                if (currentMember[key2] != null)
+                                if (!localMap.ContainsKey(key2))
                                 {
-                                    if (currentMember[key2].Equals("name"))
+                                    localMap.Add(key, currentMember[key]);
+
+                                    switch (key2)
                                     {
-                                        if (currentMember[key2].type == JSONObject.Type.STRING)
-                                        {
-                                            Debug.Log("Got newMember name: " + currentMember[key2]);
-                                            newMember.name = currentMember[key2].ToString();
-                                        }
-                                        else
-                                        {
-                                            Debug.Log("False Datatype " + jsonDiscussion[key2]);
-                                        }
-                                    }
-                                    else if (currentMember[key2].Equals("title"))
-                                    {
-                                        if (currentMember[key2].type == JSONObject.Type.STRING)
-                                        {
-                                            Debug.Log("Got newMember title: " + currentMember[key2]);
-                                            newMember.title = currentMember[key2].ToString();
-                                        }
-                                        else
-                                        {
-                                            Debug.Log("False Datatype " + jsonDiscussion[key2]);
-                                        }
-                                    }
-                                    else if (currentMember[key2].Equals("role"))
-                                    {
-                                        if (currentMember[key2].type == JSONObject.Type.STRING)
-                                        {
-                                            Debug.Log("Got newMember role: " + currentMember["role"]);
-                                            newMember.role = currentMember[key2].ToString();
-                                        }
-                                        else
-                                        {
-                                            Debug.Log("False Datatype " + jsonDiscussion[key2]);
-                                        }
-                                    }
-                                    else if (currentMember[key2].Equals("placenumber"))
-                                    {
-                                        if (currentMember[key2].type == JSONObject.Type.NUMBER)
-                                        {
-                                            Debug.Log("Got newMember placenumber: " + currentMember[key2]);
-                                            newMember.placenumber = (int)currentMember[key2].n;
-                                        }
-                                        else
-                                        {
-                                            Debug.Log("False Datatype " + jsonDiscussion[key2]);
-                                        }
+                                        case "name":
+                                            if (localMap2[key2].type == JSONObject.Type.STRING)
+                                            {
+                                                Debug.Log("Got Title: " + localMap2[key2]);
+                                                newMember.name = localMap2[key2].ToString();
+                                            }
+                                            else
+                                            {
+                                                Debug.Log("False Datatype " + localMap2[key2]);
+                                            }
+                                            break;
+
+
+                                        case "title":
+                                            if (localMap2[key2].type == JSONObject.Type.STRING)
+                                            {
+                                                Debug.Log("Got Title: " + localMap2[key2]);
+                                                newMember.title = localMap2[key2].ToString();
+                                            }
+                                            else
+                                            {
+                                                Debug.Log("False Datatype " + localMap2[key2]);
+                                            }
+                                            break;
+
+                                        case "role":
+                                            if (localMap2[key2].type == JSONObject.Type.STRING)
+                                            {
+                                                Debug.Log("Got Role: " + localMap2[key2]);
+                                                newMember.role = localMap2[key2].ToString();
+                                            }
+                                            else
+                                            {
+                                                Debug.Log("False Datatype " + localMap2[key2]);
+                                            }
+                                            break;
+
+                                        case "placenumber":
+                                            if (localMap2[key2].type == JSONObject.Type.NUMBER)
+                                            {
+                                                Debug.Log("Got Duration: " + localMap2[key2]);
+                                                discussion.duration = (int)localMap2[key2].n;
+                                            }
+                                            else
+                                            {
+                                                Debug.Log("False Datatype " + localMap2[key2]);
+                                            }
+                                            break;
                                     }
                                     memberList.Add(newMember);
                                 }
                                 else
                                 {
-                                    Debug.Log("UnknownTag: " + jsonDiscussion[key2].ToString());
+                                    Debug.Log("UnknownTag: ");
                                 }
                             }
                         }
                     }
-                }
-                else
-                {
-                    Debug.Log("UnknownTag: " + jsonDiscussion[key].ToString());
-                }
+                    break;
             }
+
         }
         return discussion;
     }
