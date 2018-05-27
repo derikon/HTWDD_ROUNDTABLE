f87d4c6a7e67827ed6987512c86e5734a2b0403b
Dennis <wittchen@htw-dresden.de>
2018-05-27 15:33:03 +0200

    MOD: include more event handlers

--- SOCKET_SERVER/src/server.js
+++ SOCKET_SERVER/src/server.js
@@ -30,6 +30,46 @@ socket_io.on("connection", (socket) => {
 				console.log("received: " + JSON.stringify(data));
 				socket.broadcast.emit("discussion", data);
 			break;
+			case "new_discussion":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("new_discussion", data);
+			break;
+			case "start_discussion":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("start_discussion", data);
+			break;
+			case "end_discussion":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("end_discussion", data);
+			break;
+			case "remaining_time":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("remaining_time", data);
+			break;
+			case "start_pause":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("start_pause", data);
+			break;
+			case "end_pause":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("end_pause", data);
+			break;
+			case "topic":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("topic", data);
+			break;
+			case "silence":
+				console.log("received topic: " + data.topic);
+				console.log("received: " + JSON.stringify(data));
+				socket.broadcast.emit("silence", data);
+			break;
 			default:
 				console.log("received unknown topic: " + data.topic);
 				console.log("received: " + JSON.stringify(data));
