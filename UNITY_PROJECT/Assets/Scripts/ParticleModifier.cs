ac5f04cf7abbc1592668664a437fc0cfc8b71f36
Dennis <wittchen@htw-dresden.de>
2018-06-07 15:38:31 +0200

    ADD: controller for particles

---
+++ UNITY_PROJECT/Assets/Scripts/ParticleModifier.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleModifier : MonoBehaviour {
+
+    private ParticleSystem particleSystem;
+    private ParticleSystem.NoiseModule noiseModifier;
+    private ParticleSystem.ColorOverLifetimeModule colorModifier;
+    private ParticleSystem.MinMaxGradient defaultGradient;
+    private Gradient actionGradient;
+
+	void Start () {
+        this.particleSystem = GetComponent<ParticleSystem>();
+        this.colorModifier = this.particleSystem.colorOverLifetime;
+        this.noiseModifier = this.particleSystem.noise;
+        this.defaultGradient = this.colorModifier.color;
+        this.actionGradient = new Gradient();
+        this.actionGradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.yellow, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) }
+        );
+	}
+	
+
+	void Update () {
+        if (Input.GetKey(KeyCode.P)) {
+            this.noiseModifier.positionAmount = .2f;
+
+
+            //colorModifier.color = this.actionGradient;
+
+        } else {
+            this.noiseModifier.positionAmount = 0f;
+            //this.colorModifier.color = this.defaultGradient;
+        }
+	}
+}
