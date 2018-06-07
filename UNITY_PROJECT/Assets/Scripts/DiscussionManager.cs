dd236a648f9fbaa4542c7db6da38a96bef9d3351
Kay <kaybode@gmx.de>
2018-06-07 12:42:02 +0200

    OnPause - Pause Animation - Pause DiscussionLogic

--- UNITY_PROJECT/Assets/Scripts/DiscussionManager.cs
+++ UNITY_PROJECT/Assets/Scripts/DiscussionManager.cs
@@ -9,16 +9,20 @@ public class DiscussionManager : MonoBehaviour
     public Text TabletText;
 
     private List<GameObject> discussionMembers = new List<GameObject>();
+    public GameObject pauseObject;
+    public GameObject discussionObject;
 
     // Use this for initialization
     void Start()
     {
-        if (TabletText == null) {
+        if (TabletText == null)
+        {
             TabletText = GameObject.FindGameObjectWithTag("TabletText").GetComponent<Text>();
         }
     }
 
-    public void FindAllPlayers() {
+    public void FindAllPlayers()
+    {
         if (discussionMembers != null && discussionMembers.Count > 0) return;
 
         discussionMembers.AddRange(GameObject.FindGameObjectsWithTag("Player"));
@@ -30,5 +34,17 @@ public class DiscussionManager : MonoBehaviour
         {
             discussionMembers[i].GetComponent<TextMesh>().text = discussion.memberList[i].name;
         }
+    }
+
+    public void OnStartPause()
+    {
+        discussionObject.SetActive(false);
+        pauseObject.SetActive(true);
+    }
+
+    public void OnEndPause()
+    {
+        pauseObject.SetActive(false);
+        discussionObject.SetActive(true);
     }
 }
