fa83c4096fa1792d5d58dda6a25ff28bd9d4a7b5
Workstation <stefan.rie@t-online.de>
2018-06-07 21:25:29 +0200

    add 3DText, Rotate_around.cs, maby for Topic

---
+++ UNITY_PROJECT/Assets/Scripts/Rotate_around.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Rotate_around : MonoBehaviour {
+    public GameObject cube;
+    public float fireRate = 5.0f;
+    private float nextFire = 0.0f;
+    public float smooth = 1f;
+
+    private Vector3 targetAngles;
+    private int i = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextFire)
+        {
+            transform.RotateAround(cube.transform.position, new Vector3(0.0f, 0.0f, 0.1f), 100 * Time.deltaTime);
+            nextFire = Time.time - fireRate;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.S)) // some condition to rotate 180
+
+        //    targetAngles = transform.eulerAngles + 180f * Vector3.down; // what the new angles should be
+
+
+        //    transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, smooth * Time.deltaTime); // lerp to new angles
+        //    i++;
+
+
+
+    }
+}
