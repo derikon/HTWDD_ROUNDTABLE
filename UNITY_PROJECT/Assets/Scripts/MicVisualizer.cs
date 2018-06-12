948ea9f06c0ff2579522cb8acb34f8a962ce89b1
FabeCraft <fabecraft@googlemail.com>
2018-06-12 10:24:08 +0200

    added particle system to player prefab

--- UNITY_PROJECT/Assets/Scripts/MicVisualizer.cs
+++ UNITY_PROJECT/Assets/Scripts/MicVisualizer.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class MicVisualizer : MonoBehaviour
 {
     private MicControlC micControl;
+    private VoiceParticleSystem voiceParticleSystem;
 
     public float[] FreqBands = new float[8];
+    float average = 0;
 
     // Use this for initialization
     void Start()
     {
         micControl = GetComponentInChildren<MicControlC>();
+        voiceParticleSystem = GetComponentInChildren<VoiceParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MakeFreqBands();
+        MakeItRain();
     }
 
     private void MakeFreqBands()
@@ -29,12 +33,8 @@ public class MicVisualizer : MonoBehaviour
 
         for (int i = 0; i < 8; i++)
         {
-            float average = 0;
+            average = 0;
             int sampleCount = (int)Mathf.Pow(2, i) * 2;
-            //if (i == 7)
-            //{
-            //    sampleCount += 2;
-            //}
 
             Debug.Log("SampleCount: " + sampleCount);
 
@@ -51,5 +51,47 @@ public class MicVisualizer : MonoBehaviour
 
             FreqBands[i] = average * .1f;
         }
+    }
+
+    private void MakeItRain()
+    {
+        int count = 0;
+
+        var spectrum = micControl.spectrumData;
+        int spectrumLength = micControl.spectrumData.Length;
+
+        for (int i = 0; i < 8; i++)
+        {
+            average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            Debug.Log("SampleCount: " + sampleCount);
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                if (count < spectrumLength)
+                {
+                    average += micControl.spectrumData[count] * (count + 1);
+                }
+                count++;
+            }
+
+            average /= count;
+        }
+
+        //if (average > average - 1)
+        //{
+        //    Debug.Log("MAKE IT RAIN BABY!");
+        //    if (voiceParticleSystem != null)
+        //    {
+        //        if (!voiceParticleSystem.NoiseSystem.isPlaying)
+        //            voiceParticleSystem.NoiseSystem.Play();
+        //    }
+        //    else
+        //    {
+        //        if (voiceParticleSystem.NoiseSystem.isPlaying)
+        //            voiceParticleSystem.NoiseSystem.Stop();
+        //    }
+        //}
     }
 }
