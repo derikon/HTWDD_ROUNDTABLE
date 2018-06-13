582a9f07d6d9c48d7e27362581a7863c7f18a4c3
Dennis <wittchen@htw-dresden.de>
2018-06-13 16:34:06 +0200

    MOD: fix final rotation

--- UNITY_PROJECT/Assets/Scripts/NameSpinner.cs
+++ UNITY_PROJECT/Assets/Scripts/NameSpinner.cs
@@ -14,17 +14,19 @@ public class NameSpinner : MonoBehaviour {
     private float _alphaCum;
     private Vector3 _zAxis;
     private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
     private bool _rotationFinished;
     private bool _fadeOutFinished;
     private TextMesh _textMesh;
     private Color _textColor;
 
-    
+
     void Start() {
         _alphaCum = 1f;
         _angleCum = 0f;
         _zAxis = Vector3.forward;
         _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
         _rotationFinished = false;
         _textMesh = GetComponent<TextMesh>();
         _textColor = _textMesh.color;
@@ -62,7 +64,7 @@ public class NameSpinner : MonoBehaviour {
     
     private void Restore() {
         transform.position = _initialPosition;
-        transform.rotation = Quaternion.identity;
+        transform.rotation = _initialRotation;
         _angleCum = 0f;
         _alphaCum = 1f;
         _textColor.a = 1f;
